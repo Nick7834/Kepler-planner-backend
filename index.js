64a86a52d4302aabba09b267aa31fd16e4edfb17
Nikita <Hellreg124@gmail.com>
2024-05-25 00:11:11 +0300

    new fix back

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ import { UseController, TaskController, FolderController } from './controllers/i
 import cors from 'cors';
 import fs from 'fs';
 import User from './modules/User.js';
+import rateLimit from 'express-rate-limit'; 
 
 // db 
 
 mongoose.connect(
-    process.env.MONGODB_URL,
+  process.env.MONGODB_URL,
 ).then(() => console.log('DB OK'))
 .catch((err) => console.log('DB error', err));
 
@@ -21,6 +22,19 @@ app.use(cors());
 app.use(express.json());
 app.use('/backgrounds', express.static('backgrounds'));
 app.use('/avatars', express.static('avatars'));
+
+const readLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 минута
+  max: 150, // до 120 запросов в минуту
+  message: "Слишком много запросов, попробуйте позже."
+});
+
+// Лимит для изменения данных
+const modifyLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 минута
+  max: 100, // до 60 запросов в минуту
+  message: "Слишком много запросов, попробуйте позже."
+});
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -84,27 +98,27 @@ app.get('/auth/me', checkAuth, UseController.getMe);
 
 // app
 
-app.post('/tasks/today', checkAuth, FolderController.createTodayTask); // +
-app.post('/tasks', checkAuth, FolderController.createTask)
-app.post('/tasks/today/add', checkAuth, FolderController.addExistingTaskToToday); // new
-app.post('/tasks/week', checkAuth, FolderController.createTaskForDayOfWeek); // + но в разработке ----------------
-app.post('/folders', checkAuth, FolderController.createFolder); //  +
-app.post('/folders/:folderId/tasks', checkAuth, FolderController.createTaskInFolder); // +
+app.post('/tasks/today', checkAuth, modifyLimiter, FolderController.createTodayTask); 
+app.post('/tasks', checkAuth, modifyLimiter, FolderController.createTask);
+app.post('/tasks/today/add', checkAuth, modifyLimiter, FolderController.addExistingTaskToToday); 
+app.post('/tasks/week', checkAuth, modifyLimiter, FolderController.createTaskForDayOfWeek); 
+app.post('/folders', checkAuth, modifyLimiter, FolderController.createFolder); 
+app.post('/folders/:folderId/tasks', checkAuth, modifyLimiter, FolderController.createTaskInFolder); 
 
-app.get('/tasks/:taskId', checkAuth, FolderController.getTaskById);
-app.get('/folders/:folderId', checkAuth, FolderController.getFolderById);
-app.get('/allFolders', checkAuth, FolderController.getAllFolders);
-app.get('/weekdays', checkAuth, FolderController.getAllWeekDays); /// ------------
-app.get('/tasks/today/all', checkAuth, FolderController.allTasksToday);
-app.get('/tasks', checkAuth, FolderController.allTasks); // +
-app.get('/alltasks/search', checkAuth, FolderController.searchTasks);
+app.get('/tasks/:taskId', checkAuth, readLimiter, FolderController.getTaskById);
+app.get('/folders/:folderId', checkAuth, readLimiter, FolderController.getFolderById);
+app.get('/allFolders', checkAuth, readLimiter, FolderController.getAllFolders);
+app.get('/weekdays', checkAuth, readLimiter, FolderController.getAllWeekDays); 
+app.get('/tasks/today/all', checkAuth, readLimiter, FolderController.allTasksToday);
+app.get('/tasks', checkAuth, readLimiter, FolderController.allTasks); 
+app.get('/alltasks/search', checkAuth, readLimiter, FolderController.searchTasks);
 
-app.patch('/tasks/:taskId', checkAuth, FolderController.updateTask); // +
-app.patch('/folders/:folderId', checkAuth, FolderController.updateFolder); // +
+app.patch('/tasks/:taskId', checkAuth, modifyLimiter, FolderController.updateTask); 
+app.patch('/folders/:folderId', checkAuth, modifyLimiter, FolderController.updateFolder); 
 
-app.delete('/tasks/:taskId', checkAuth, FolderController.deleteTask); // + 
-app.delete('/folders/:folderId', checkAuth, FolderController.deleteFolder); // +
-app.delete('/tasks/today/remove/:taskId', checkAuth, FolderController.removeTaskFromToday);
+app.delete('/tasks/:taskId', checkAuth, modifyLimiter, FolderController.deleteTask); 
+app.delete('/folders/:folderId', checkAuth, modifyLimiter, FolderController.deleteFolder); 
+app.delete('/tasks/today/remove/:taskId', checkAuth, modifyLimiter, FolderController.removeTaskFromToday);
 
 // host
 
